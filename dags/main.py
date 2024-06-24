94b06e10a1d52fdfad2256c299245055c5c265f2
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-06-24 20:41:10 +0200

    pretest

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,8 @@
 from collections.abc import MutableMapping
 from operator import index
 from os.path import isfile
+import urllib
+from urllib.error import URLError
 # import flatdict
 from pathlib import Path
 import datetime
@@ -16,6 +18,7 @@ import requests
 import json
 from uuid import uuid4
 from random import randint
+from time import sleep
 
 # AIRFLOW_VAR_PROJECT_HOME='$HOME/Projects/BIProjects'
 
@@ -105,7 +108,10 @@ def extract_from_combined_csv():
 
 def extract_from_csv():
     global global_run_id
-    urlretrieve("https://github.com/11jolek11/BIProject/raw/main/data.zip", "./data.zip")
+    try:
+        urlretrieve("https://github.com/11jolek11/BIProject/raw/main/data.zip", "./data.zip")
+    except URLError:
+        print("No connection")
     with zipfile.ZipFile("./data.zip", 'r') as zip_ref:
         zip_ref.extractall(".")
     data_dict = custom_data
@@ -157,10 +163,12 @@ def unify_date_format(id):
 
     extracted_data.to_csv(f"{staging_area_path}/{id}.csv")
     print(f"{staging_area_path}/{id}.csv")
+
     return id
 
 
 def get_coordinates(id):
+    # FIXME(11jolek11): Read adds 2x extra Unnamed columns
     extracted_data = pd.read_csv(f"{staging_area_path}/{id}.csv")
     locations = extracted_data["Address"]
     # print(locations)
@@ -218,16 +226,17 @@ def extract_weather(id):
     # TODO(11jolek11): What to do when lon and lat values are missing (when lon and lat == 0.0 ) because of fail in prev node?
     # TODO(11jolek11): What to do when lon and lat values are missing because of lack data on OpenWeather portal?
     extracted_data = pd.read_csv(f"{staging_area_path}/{id}.csv")
+    ttt = list(extracted_data.columns).copy()
     # TODO(11jolek11): Add checks,, if lat and lon in request == lat and lon in response
     expected_columns = ["lat", "lon", "date", "cloud_cover_afternoon", "humidity_afternoon",
-                                       "precipitation_total", "pressure_afternoon", "temperature", "wind_max_speed", "wind_max_direction"]
+                                       "precipitation_total", "pressure_afternoon", "temperature_max", "wind_max_speed", "wind_max_direction"]
     weather_df = pd.DataFrame(columns=expected_columns)
     lats = extracted_data["Lat"].values
     lons = extracted_data["Lon"].values
     df_list = []
 
     for data_idx in extracted_data.index:
-        print(f"Getting {data_idx} weather")
+        # print(f"Getting {data_idx} weather")
         target_date = extracted_data.loc[data_idx, "Incident Date"]
         weather_url = f'https://api.openweathermap.org/data/3.0/onecall/day_summary?lat={lats[data_idx]}&lon={lons[data_idx]}&date={target_date}&appid={openweather_api_key}'
         default_values = [lats[data_idx], lats[data_idx], extracted_data.loc[data_idx, "Incident Date"], 0, 0, 0, 0, 0.0, 0.0, 0]
@@ -241,14 +250,20 @@ def extract_weather(id):
                     resp = requests.get(url=weather_url)
                     if resp.status_code == 200 and resp.json():
                         temp_dict = resp.json().copy()
+                        # print(temp_dict["temperature"])
                         keys_collection = list(temp_dict.keys()).copy()
                         for key in keys_collection:
+                            # print(f"{key}")
+                            # FIXME(11jolek11): Fix this!
                             if key not in weather_df.columns:
-                                del temp_dict[key]
+                                if key != "temperature":
+                                    del temp_dict[key]
 
                         for key, value in flatten(temp_dict).items():
+                            # FIXME(11jolek11): Fix this!
+                            if (key.startswith("temperature") and not key.endswith("max")) or key == "temperature":
+                                continue
                             temp_weather[key] = [value]
-
                         if temp_weather["lat"] != lats[data_idx] or temp_weather["lon"] != lons[data_idx]:
                             print("Error {} -- {} || {} -- {} ".format(temp_weather["lat"], lats[data_idx], temp_weather["lon"], lons[data_idx]))
 
@@ -261,6 +276,8 @@ def extract_weather(id):
 
                 except requests.exceptions.ConnectionError:
                     print("Server WAF blocked connection")
+                except URLError:
+                    print("No connection")
 
             else:
                 print("Get from cache")
@@ -269,23 +286,30 @@ def extract_weather(id):
                         # Adding value as [value]
                         temp_weather[key] = [value]
 
-        print(temp_weather)
+        # print(temp_weather.keys())
         if list(temp_weather.keys()) == expected_columns:
             df_list.append(pd.DataFrame(temp_weather, columns=expected_columns))
         else:
+            # pass
             print("Columns error")
 
     weather_df = pd.concat([*df_list, weather_df], ignore_index=True)
-    extracted_data.to_csv(f"{staging_area_path}/{id}.csv")
+    # extracted_data.to_csv(f"{staging_area_path}/{id}.csv")
     weather_id = create_file_id(global_run_id)
     weather_df.to_csv(f"{staging_area_path}/{weather_id}.csv")
-    print(f"Path: {staging_area_path}/{weather_id}.csv")
+
+    print(f"Weather: reading {ttt}")
+    print(f"Path: weather {staging_area_path}/{weather_id}.csv, Extracted {staging_area_path}/{id}.csv")
     return {"extracted": id, "weather": weather_id}
 
 
 def add_count_or_city(ids_dict):
     id = ids_dict["extracted"]
-    extracted_data = pd.read_csv(f"{staging_area_path}/{id}")
+    extracted_data = pd.read_csv(f"{staging_area_path}/{id}.csv")
+    print(f"Reading: {extracted_data.columns}")
+    location_df_columns = ["Address", "Lat", "Lon", "State"]
+    columns_to_drop = ["Address", "Lat", "Lon", "City Or County", "Operations"]
+    location_df = extracted_data[location_df_columns].copy()
     # FIXME(11jolek11): Fix column name
     mixed_data = extracted_data["City Or County"]
 
@@ -299,19 +323,48 @@ def add_count_or_city(ids_dict):
     for idx in mixed_data.index:
         if str(mixed_data[idx]).lower() in cities:
             temp_city_list.append(mixed_data.loc[idx])
-            temp_county_list.append(cities_file['county_name'])
+            # FIXME(11jolek11):
+            # temp_county_list.append(cities_file.loc[cities_file["city"] == str(mixed_data[idx])].loc[0, "county_name"])
+            candidates = list(cities_file.loc[cities_file["city"] == str(mixed_data[idx])]["county_name"].values)
+            if candidates:
+                temp_county_list.append(candidates[0])
+            else:
+                temp_county_list.append("")
+
         else:
             temp_city_list.append("")
             temp_county_list.append(mixed_data[idx])
 
-    extracted_data["City"] = temp_city_list
-    extracted_data["County"] = temp_county_list
+    location_df["City"] = temp_city_list
+    location_df["County"] = temp_county_list
 
+    extracted_data.drop(columns_to_drop, axis="columns", inplace=True)
+
+    # extracted_data.drop(["City Or County"], inplace=True)
+    print(f"Writing {extracted_data.columns}")
+    extracted_data.to_csv(f"{staging_area_path}/{id}.csv")
+
+    location_id = create_file_id(global_run_id)
+    location_df.to_csv(f"{staging_area_path}/{location_id}.csv")
+    ids_dict["location"] = location_id
+    print(ids_dict)
+    return ids_dict
+
+def year_extractor(date: str, delimeter="-", year_loc=0):
+    return date.split(delimeter)[year_loc]
+
+def add_temp_year_from_date(ids_dict):
+    id = ids_dict["extracted"]
+    extracted_data = pd.read_csv(f"{staging_area_path}/{id}.csv")
+    dates = list(map(year_extractor, list(extracted_data["Incident Date"].values)))
+    extracted_data["TEMP_YEAR"] = dates
+    print(f"Writing {extracted_data.columns}")
     extracted_data.to_csv(f"{staging_area_path}/{id}.csv")
     return ids_dict
 
 
 if __name__ == "__main__":
     extract_weather(get_coordinates(unify_date_format(extract_from_csv())))
+    # add_temp_year_from_date(add_count_or_city(extract_weather(get_coordinates(unify_date_format(extract_from_csv())))))
     # get_coordinates(unify_date_format(extract_from_csv()))
 
